6b69e4d2e16a0c9bae4ab9cf39872d4c908459b7
Andrei Alves <138537998+andrei-alves@users.noreply.github.com>
2024-04-08 21:09:47 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone do https://tabnews.com.br desenvolvido para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ei gata 😎, tá afim de uma pizza hoje? 🍕</h1>
+  return <h1>Ei gata 😎, tá afim de uma pizza hoje? 🍕</h1>;
 }
 
 export default Home;
